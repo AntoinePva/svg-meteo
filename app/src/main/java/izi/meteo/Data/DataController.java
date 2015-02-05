5064eea494474b5c3fa38ca4486710e748c00b24
Bitafoot <apromerova@gmail.com>
2015-02-05 14:50:12 +0100

    Base

--- app/src/main/java/izi/meteo/Data/DataController.java
+++ app/src/main/java/izi/meteo/Data/DataController.java
@@ -21,11 +21,6 @@ import java.net.URLConnection;
  * Created by Antoine on 04/02/2015.
  */
 public class DataController extends AsyncTask<Void, Void, Void> {
-<<<<<<< HEAD
-    //    private static final String weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=city,country#sthash.Q7DzQboM.dpuf";
-
-=======
->>>>>>> origin/Antoine
 
     @Override
     protected void onPreExecute() {
@@ -33,14 +28,13 @@ public class DataController extends AsyncTask<Void, Void, Void> {
     }
 
     @Override
-<<<<<<< HEAD
+
     protected Void doInBackground(Void... params) {
         return null;
     }
 
     @Override
-=======
->>>>>>> origin/Antoine
+
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
     }
@@ -49,12 +43,5 @@ public class DataController extends AsyncTask<Void, Void, Void> {
     protected void onProgressUpdate(Void... values) {
         super.onProgressUpdate(values);
     }
-<<<<<<< HEAD
-=======
 
-    @Override
-    protected Void doInBackground(Void... params) {
-        return null;
-    }
->>>>>>> origin/Antoine
 }
