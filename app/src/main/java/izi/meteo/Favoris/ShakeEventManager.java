8e0a03de798e6c049256bda6255dd654f6dd9062
tomouxx <duhayon.thomas@gmail.com>
2015-02-05 17:34:22 +0100

    fav

--- app/src/main/java/izi/meteo/Favoris/ShakeEventManager.java
+++ app/src/main/java/izi/meteo/Favoris/ShakeEventManager.java
@@ -5,3 +5,13 @@ package izi.meteo.Favoris;
  */
 public class ShakeEventManager {
 }
+
+public void init(Context ctx) {
+    sManager = (SensorManager)  ctx.getSystemService(Context.SENSOR_SERVICE);
+    s = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    register();
+}
+
+public void register() {
+        sManager.registerListener(this, s, SensorManager.SENSOR_DELAY_NORMAL);
+}
