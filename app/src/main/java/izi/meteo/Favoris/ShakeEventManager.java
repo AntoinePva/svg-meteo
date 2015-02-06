0fa3e4309ee86dc5b9879dbe1d5634ad40f09ace
tomouxx <duhayon.thomas@gmail.com>
2015-02-06 17:19:00 +0100

    shake

--- app/src/main/java/izi/meteo/Favoris/ShakeEventManager.java
+++ app/src/main/java/izi/meteo/Favoris/ShakeEventManager.java
@@ -1,19 +1,33 @@
 package izi.meteo.Favoris;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+
 /**
  * Created by thomas on 05/02/2015.
  */
-public class ShakeEventManager {
+public class ShakeEventManager extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
 }
 
-public void init(Context ctx) {
-    sManager = (SensorManager)  ctx.getSystemService(Context.SENSOR_SERVICE);
-    s = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-    register();
-}
 
-public void register() {
-        sManager.registerListener(this, s, SensorManager.SENSOR_DELAY_NORMAL);
-}
+   // public void init(Context ctx) {
+   // sManager = (SensorManager)  ctx.getSystemService(Context.SENSOR_SERVICE);
+   // s = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+   // register();
+//}
+
+//public void register() {
+ /*       sManager.registerListener(this, s, SensorManager.SENSOR_DELAY_NORMAL);
+}*/
+
+
 
 //http://www.javacodegeeks.com/2014/04/android-shake-to-refresh-tutorial.html
